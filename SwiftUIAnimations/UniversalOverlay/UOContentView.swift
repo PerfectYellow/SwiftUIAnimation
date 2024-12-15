4ea144cb0c93fbf80053818805f4f361f67c4d83
Mohammad Afshar <neo.mohammad.afshar@gmail.com>
2024-12-15 14:47:57 +0330

    Universal Overlay, (Kavsoft-GOAT)

---
+++ SwiftUIAnimations/UniversalOverlay/UOContentView.swift
@@ -0,0 +1,18 @@
+//
+//  UOContentView.swift
+//  SwiftUIAnimations
+//
+//  Created by Mohammad Afshar on 15/12/2024.
+//
+
+import SwiftUI
+
+struct UOContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UOContentView()
+}
---
+++ SwiftUIAnimations/UniversalOverlay/UniversalOverlay.swift
@@ -0,0 +1,18 @@
+//
+//  UniversalOverlay.swift
+//  SwiftUIAnimations
+//
+//  Created by Mohammad Afshar on 15/12/2024.
+//
+
+import SwiftUI
+
+struct UniversalOverlay: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UniversalOverlay()
+}
